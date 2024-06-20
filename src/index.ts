e69efa606a2be0ac445d83c539d23000c7cb5158
rinnegard <victor.rinnegard@hotmail.com>
2024-06-20 10:21:28 +0200

    Added basic crud functions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,52 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
+async function createStudent(name: string, email: string) {
+    const student = await prisma.student.create({
+        data: {
+            name: name,
+            email: email
+        }
+    })
+    console.log(student);
+}
 
+async function addCourse(name: string, teacher: string) {
+    const course = await prisma.course.create({
+        data: {
+            name: name,
+            teacherId: teacher
+        }
+    })
+}
+
+async function addStudentToCourse(courseName: string, studentEmail: string) {
+    const course = await prisma.course.findFirst({
+        where: {
+            name: courseName
+        }
+    })
+
+    const student = await prisma.student.findFirst({
+        where: {
+            email: studentEmail
+        }
+    })
+    await prisma.course.update({
+        where: {
+            id: course?.id
+        },
+        data: {
+            students: {
+                connect: {
+                    id: student?.id
+                }
+            }
+        }
+    })
+}
+
+// createStudent("Maria", "maria@lexicon.se")
+// addCourse("Programming", "clxmy5dgu0003s8shmc7c224g")
+// addStudentToCourse("Programming", "maria@lexicon.se")
 
