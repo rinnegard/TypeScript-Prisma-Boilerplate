104f4546808e680bad0f83c1a1f11dc706570427
rinnegard <victor.rinnegard@hotmail.com>
2024-06-20 11:43:08 +0200

    Added more crud

--- src/index.ts
+++ src/index.ts
@@ -5,48 +5,95 @@ async function createStudent(name: string, email: string) {
     const student = await prisma.student.create({
         data: {
             name: name,
-            email: email
-        }
-    })
-    console.log(student);
+            email: email,
+        },
+    });
+    console.table(student);
 }
 
-async function addCourse(name: string, teacher: string) {
+async function createTeacher(name: string, email: string) {
+    const teacher = await prisma.teacher.create({
+        data: {
+            name: name,
+            email: email,
+        },
+    });
+    console.table(teacher);
+}
+
+async function createCourse(name: string, teacher: string) {
     const course = await prisma.course.create({
         data: {
             name: name,
-            teacherId: teacher
-        }
-    })
+            teacherId: teacher,
+        },
+    });
 }
 
 async function addStudentToCourse(courseName: string, studentEmail: string) {
     const course = await prisma.course.findFirst({
         where: {
-            name: courseName
-        }
-    })
+            name: courseName,
+        },
+    });
 
-    const student = await prisma.student.findFirst({
+    const student = await prisma.student.findUnique({
         where: {
-            email: studentEmail
-        }
-    })
+            email: studentEmail,
+        },
+    });
     await prisma.course.update({
         where: {
-            id: course?.id
+            id: course?.id,
         },
         data: {
             students: {
                 connect: {
-                    id: student?.id
-                }
-            }
-        }
-    })
+                    id: student?.id,
+                },
+            },
+        },
+    });
+}
+
+async function getAllCourses() {
+    const courses = await prisma.course.findMany({
+        include: {
+            students: true,
+        },
+    });
+    return courses;
+}
+
+async function getStudents(search?: string) {
+    const students = await prisma.student.findMany({
+        where: {
+            OR: [
+                {
+                    name: {
+                        contains: search,
+                    },
+                },
+                {
+                    email: {
+                        contains: search,
+                    },
+                },
+            ],
+        },
+    });
+    console.table(students);
 }
 
 // createStudent("Maria", "maria@lexicon.se")
-// addCourse("Programming", "clxmy5dgu0003s8shmc7c224g")
+// createStudent("Victor", "victor@abc.se");
+// createTeacher("Nalini", "nalini@lexicon.se")
+// createCourse("Programming", "clxmy5dgu0003s8shmc7c224g")
 // addStudentToCourse("Programming", "maria@lexicon.se")
-
+// getStudents("lexicon");
+getAllCourses().then(async (courses) => {
+    courses.forEach((course) => {
+        console.log(course.name);
+        console.table(course.students);
+    });
+});
